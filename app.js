6005cfa3f28d8d651cc4d1df291aa3b15b0c0de4
SpencerTower <spencerjtower1@gmail.com>
2021-11-01 16:00:44 -0700

    adds object literals for locations

---
+++ app.js
@@ -0,0 +1,60 @@
+'use strict';
+
+let seattle = {
+    minCust: 23,
+    maxCust: 65,
+    avgCookiesSold: 6.3,
+    hourlyCust = 0,
+    hourlyCookiesSold = 0,
+    assignHourlyCust: function () {
+        this.hourlyCust = randomInRange(23, 65);
+    },
+}
+
+// calc and store cookies purchases for each hour
+function 
+
+
+
+
+/*
+let tokyo = {
+    minCust: 3,
+    maxCust: 24,
+    avgCookiesSold: 1.2,
+    hourlyCust = 0,
+    assignHourlyCust: function () {
+        this.hourlyCust = randomInRange(3, 24);
+    }
+}
+
+let dubai = {
+    minCust: 11,
+    maxCust: 38,
+    avgCookiesSold: 3.7,
+    hourlyCust = 0,
+    assignHourlyCust: function () {
+        this.hourlyCust = randomInRange(11, 38);
+    }
+}
+
+let paris = {
+    minCust: 20,
+    maxCust: 38,
+    avgCookiesSold: 2.3,
+    hourlyCust = 0,
+    assignHourlyCust: function () {
+        this.hourlyCust = randomInRange(20, 38);
+    }
+}
+
+let lima = {
+    minCust: 2,
+    maxCust: 16,
+    avgCookiesSold: 4.6,
+    hourlyCust = 0,
+    assignHourlyCust: function () {
+        this.hourlyCust = randomInRange(2, 16);
+    }
+}
+*/

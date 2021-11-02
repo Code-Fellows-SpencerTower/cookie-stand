ca18d0bea61bf3ba9168e2f270ab0ccdc6a107cd
SpencerTower <spencerjtower1@gmail.com>
2021-11-01 17:21:04 -0700

    adds randomInRange()

--- app.js
+++ app.js
@@ -4,15 +4,47 @@ let seattle = {
     minCust: 23,
     maxCust: 65,
     avgCookiesSold: 6.3,
-    hourlyCust = 0,
-    hourlyCookiesSold = 0,
+    hourlyCust: 0,
+    hourlyCookiesSold: 0,
     assignHourlyCust: function () {
         this.hourlyCust = randomInRange(23, 65);
     },
+    assignCookiesSoldPerHour: function () {
+        this.hourlyCookiesSold = avgCookiesSold * hourlyCust;
+    },
+    cookiesSoldEachHour: [],
+    locationHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+    totalCookiesSold: 0,
+    assignCookiesSoldEachHour: function () {
+        
+    }
+
+}
+
+// from class demo
+function randomInRange(min, max) {
+    let range = max - min + 1;
+    return Math.floor(Math.random() * range) + min;
 }
 
 // calc and store cookies purchases for each hour
-function 
+function calculateCookiesSoldPerHour(location) {
+    console.log("location: " + location)
+    let hourlyCustomers = location.assignHourlyCust;
+    console.log(hourlyCustomers);
+    let cookiesSoldPerHour = location.assignCookiesSoldPerHour;
+    console.log(cookiesSoldPerHour);
+}
+
+let customers = seattle.assignHourlyCust();
+console.log("customers: " + customers);
+calculateCookiesSoldPerHour("seattle");
+
+
+
+
+
+
 
 
 

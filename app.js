52705683689ecd7b94d41bf62aa29a92ab5b4450
SpencerTower <spencerjtower1@gmail.com>
2021-11-02 14:26:16 -0700

    updates variable and function naming

--- app.js
+++ app.js
@@ -1,39 +1,27 @@
-//'use strict';
+'use strict';
 
-const seattle = {
+const seattleShop = {
     location: 'Seattle',
     minCust: 23,
     maxCust: 65,
     avgCookiesSold: 6.3,
     hourlyCust: 0,
     cookiesSold: 0,
-    cookiesSoldByHour: [10, 8],
-    //hours: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '],
-    assignHourlyCust: function () {
-        this.hourlyCust = randomInRange(23, 65);
-        return this.hourlyCust;
-    },
+    cookiesSoldPerHour: [10, 8],
+    
     // hourlyCust must be initiated first
     assignCookiesSold: function () {
-        this.cookiesSold = this.avgCookiesSold * this.hourlyCust;
-        return this.cookiesSold;
+        this.cookiesSold = this.avgCookiesSold * randomCust(this.minCust, this.maxCust)
+        return Math.ceil(this.cookiesSold); // round up
     },
-    /*
-    assignCookiesSoldByHour: function () {
-        for (let i = 0; i < this.hours.length; i += 1) {
-            this.cookiesSoldByHour.push(this.hours[i] + this.cookiesSold + " cookies");
-        }
-        return this.cookiesSoldByHour;
-    }
-    */
-
 }
 
 // from class demo
-function randomInRange(min, max) {
+function randomCust(min, max) {
     let range = max - min + 1;
     return Math.floor(Math.random() * range) + min;
 }
+
 
 
 // 1. make sure object properties work X
@@ -50,7 +38,7 @@ function randomInRange(min, max) {
 
 
 
-const hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '];
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 const divElem = document.getElementById('seattle'); // get parent element
 const ulElem = document.createElement('ul'); // make child ul element
@@ -60,7 +48,7 @@ divElem.appendChild(ulElem); // append ul to div
 
 function populateSalesList() {
     for (let i = 0; i < hours.length; i++) {
-        const listText = `${hours[i]}: ${seattle.cookiesSoldByHour[i]} cookies`;
+        const listText = `${hours[i]}: ${seattleShop.cookiesSoldByHour[i]} cookies`;
         // make child li element
         const salesLi = document.createElement('li');
         // append li to ul
@@ -73,124 +61,9 @@ populateSalesList();
 
 
 
+// calculate and store average sales per hour for the shop
+// get total sales
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 6. make function that populates empty array with hour and cookies sold in a string
-
-/*
-let hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ' , '6pm: ', '7pm: '];
-
-let cookiesSold = 5;
-
-let cookiesSoldByHour = [];
-
-function assignCookiesSoldByHour(hours, cookiesSold, cookiesSoldByHour) {
-    console.log("hours.length: "  + hours.length);
-    for (let i = 0; i < hours.length; i += 1) {
-        cookiesSoldByHour.push(hours[i] + cookiesSold);
-        console.log(cookiesSoldByHour);
-    }
-    return cookiesSoldByHour;
+for (i = 0; i < hours.length; i += 1) {
+    cookiesShop.
 }
-
-let cookiesHours = assignCookiesSoldByHour(hours, cookiesSold, cookiesSoldByHour);
-console.log(cookiesHours);
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// from class demo
-function randomInRange(min, max) {
-    let range = max - min + 1;
-    return Math.floor(Math.random() * range) + min;
-}
-
-
-
-let tokyo = {
-    minCust: 3,
-    maxCust: 24,
-    avgCookiesSold: 1.2,
-    hourlyCust = 0,
-    assignHourlyCust: function () {
-        this.hourlyCust = randomInRange(3, 24);
-    }
-}
-
-let dubai = {
-    minCust: 11,
-    maxCust: 38,
-    avgCookiesSold: 3.7,
-    hourlyCust = 0,
-    assignHourlyCust: function () {
-        this.hourlyCust = randomInRange(11, 38);
-    }
-}
-
-let paris = {
-    minCust: 20,
-    maxCust: 38,
-    avgCookiesSold: 2.3,
-    hourlyCust = 0,
-    assignHourlyCust: function () {
-        this.hourlyCust = randomInRange(20, 38);
-    }
-}
-
-let lima = {
-    minCust: 2,
-    maxCust: 16,
-    avgCookiesSold: 4.6,
-    hourlyCust = 0,
-    assignHourlyCust: function () {
-        this.hourlyCust = randomInRange(2, 16);
-    }
-}
-*/

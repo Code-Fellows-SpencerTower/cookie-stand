0a39c61f5c268110e808c636d4ba6b4e015a15fa
SpencerTower <spencerjtower1@gmail.com>
2021-11-02 16:01:38 -0700

    adds Stand object constructor, adds cookieSales() prototype method

--- app.js
+++ app.js
@@ -1,41 +1,50 @@
 'use strict';
 
-const seattleShop = {
-    location: 'Seattle',
-    minCust: 23,
-    maxCust: 65,
-    avgCookiesSold: 6.3,
-    hourlyCust: 0,
-    cookiesSold: 0,
-    cookiesSoldPerHour: [10, 8],
-    
-    // hourlyCust must be initiated first
-    assignCookiesSold: function () {
-        this.cookiesSold = this.avgCookiesSold * randomCust(this.minCust, this.maxCust)
-        return Math.ceil(this.cookiesSold); // round up
-    },
+// 1. refactor object literal into single object constructor X
+// 2. make sure object constructor works X
+// 3. make prototype function for random number of cookies per hour X
+// 4. append table to html outside of render() method
+// 5. replace lists with table - make function outside of render()
+// 6. append table to html
+// 7. make render() prototype method for table
+
+// 'this.' outside of object references the DOM
+// arrow function?
+
+
+// 1. refactor object literal into single object constructor
+function Stand(location, minCust, maxCust, avgCookieSale) {
+    this.location = location;
+    this.minCust = minCust;
+    this.maxCust = maxCust;
+    this.avgCookieSale = avgCookieSale;
+
 }
+
+// generate cookies sold per hour
+Stand.prototype.cookiesSold = function() {
+    this.cookiesSold = this.avgCookieSale * randomCust(this.minCust, this.maxCust)
+    return Math.ceil(this.cookiesSold); // round up
+}
+
+
+
+// test
+const seattle = new Stand('Seattle', 23, 65, 6.3);
+console.log(seattle);
+const seattleSales = seattle.cookiesSold();
+console.log(seattleSales);
+
+
+
+
+
 
 // from class demo
 function randomCust(min, max) {
     let range = max - min + 1;
     return Math.floor(Math.random() * range) + min;
 }
-
-
-
-// 1. make sure object properties work X
-// 2. make sure object methods work X
-// 3. make function to calculate cookies sold each hour X
-// 4. make array with location hours X
-// 5. make empty array for hours + cookies sold X
-// 6. make function that populates empty array with hour and cookies sold in a string
-// 7. add 'seattle' as id to div X
-// 8. grab parent element from html X
-// 9. add ul to parent element X
-// 10. add li to ul X
-// 11. add hour + cookies sold to each hour
-
 
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
@@ -46,6 +55,8 @@ divElem.appendChild(ulElem); // append ul to div
 
 
 
+
+/*
 function populateSalesList() {
     for (let i = 0; i < hours.length; i++) {
         const listText = `${hours[i]}: ${seattleShop.cookiesSoldByHour[i]} cookies`;
@@ -55,15 +66,14 @@ function populateSalesList() {
         ulElem.appendChild(salesLi).textContent = listText;
     }
 }
-
 populateSalesList();
-
+*/
 
 
 
 // calculate and store average sales per hour for the shop
 // get total sales
 
-for (i = 0; i < hours.length; i += 1) {
-    cookiesShop.
-}
+//for (i = 0; i < hours.length; i += 1) {
+//    cookiesShop.
+//}

7b52afb04920b928239ed8599f0d4a815da76cc1
SpencerTower <spencerjtower1@gmail.com>
2021-11-04 22:54:13 -0700

    adds submit button

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+
+
+
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 
@@ -192,6 +195,6 @@ console.log('hourlTotalSales: ' + hourlyTotalSales);
 
 }
 
-
 const tdFooterElem = document.createElement('td');
 salesTableElem.appendChild(tdFooterElem);
+

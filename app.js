8e0d0455f25cf79146201e5670a37027b421216b
SpencerTower <spencerjtower1@gmail.com>
2021-11-04 19:28:00 -0700

    updates genHourlyTotal()

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
 'use strict';
 
-// 1. render method to create and append a row for each location
-// 1. create row
-// 2. append row
-// 3. populate row
-// 4. 1st cell in row - location name
-// if i = 0 then populate with location name
-// else, populate with random
-// 5. populate rest of cells based on 
-//Stand.prototype.render = function () {}
-
-// store hours
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
 
 
 function Stand(location, minCust, maxCust, avgSale) {
@@ -20,17 +8,16 @@ function Stand(location, minCust, maxCust, avgSale) {
     this.minCust = minCust;
     this.maxCust = maxCust;
     this.avgSale = avgSale;
-
     this.hourlySales = [];
     this.dailyTotal = 0;
     this.hourlyTotal = 0;
+    this.allLocHourlyTotal = 0;
 
     this.genHourlySales();
     this.genDailyTotal();
-
 }
 
-// generate cookies sold per hour // extend to fill .hourlySales[]
+
 Stand.prototype.genHourlySales = function () {
 
     for (let i = 0; i < hours.length; i += 1) {
@@ -48,30 +35,10 @@ Stand.prototype.genDailyTotal = function () {
 
         // grab sale, add to total
         total += this.hourlySales[i];
-        // add to total
-        // ?????????????
-        console.log('RUNNING TOTAL: ' + total);
-
     }
+
     this.dailyTotal = total;
 }
-
-/*
-// add up daily sales // append to dailyTotal
-Stand.prototype.genDailyTotal = function () {
-
-    for (let i = 0; i < this.hourlySales.length; i +=1 ) {
-
-        // grab sale
-        const sale = this.hourlySales[i];
-        // add to total
-        runningTotal += sale;
-    }
-
-    this.hourlyTotal = runningTotal; 
-}
-*/
-
 
 
 const seattle = new Stand('Seattle', 23, 65, 6.3);
@@ -81,18 +48,6 @@ const paris = new Stand('Paris', 20, 38, 2.3);
 const lima = new Stand('Lima', 2, 16, 4.6);
 
 const standLocations = [seattle, tokyo, dubai, paris, lima];
-
-//seattle.genDailyTotal();
-console.log('SEATTLE DAILY TOTAL: ' + seattle.dailyTotal);
-
-
-
-
-console.log('seattle: ' + seattle.hourlySales);
-console.log('lima: ' + lima.hourlySales);
-
-// iterate through each hour
-// iterate through each location for each hour
 
 const hourlyTotalSales = [];
 
@@ -104,9 +59,7 @@ function randomCust(min, max) {
 }
 
 
-// Stand.prototype.render = function()
 
-// get sales div
 // grab div
 const salesDivElem = document.getElementById('sales');
 // create table
@@ -136,7 +89,7 @@ function headerRow() {
     // append 'Totals' header to end of row
     const totalHeaderElem = document.createElement('th');
     headerRowElem.appendChild(totalHeaderElem);
-    totalHeaderElem.textContent = 'Totals';
+    totalHeaderElem.textContent = 'Daily Location Total';
 
 }
 
@@ -146,14 +99,6 @@ Stand.prototype.render = function () {
     const standRowElem = document.createElement('tr');
     // append row
     salesTableElem.appendChild(standRowElem);
-
-    /*
-    // get stand object
-    const standObject = this.location;
-    // get stand location param
-    const standLocationParam = standObject.location;
-    console.log(standLocationParam); // test
-    */
 
     // create header
     const standHeader = document.createElement('th');
@@ -177,10 +122,13 @@ Stand.prototype.render = function () {
         salesDataElem.textContent = salesDataForHour;
 
     }
-
+    // final cell add total
+    const thTotal = document.createElement('th');
+    standRowElem.appendChild(thTotal);
+    thTotal.textContent = this.dailyTotal;
 }
 
-// make table footer row
+
 
 function footerRow() {
     // Add up hourly total
@@ -194,15 +142,15 @@ function footerRow() {
     const thCell = document.createElement('th');
     tableFooter.appendChild(thCell);
     thCell.textContent = 'Totals';
+   
+        for (let i = 0; i < hours.length; i += 1) {
+            const tdCell = document.createElement('td');
+            tableFooter.appendChild(tdCell);
 
+            // call function
 
-    // need cell for every hour (loop) for as many hours
-
-    for (let i = 0; i < hours.length; i += 1) {
-        const tdCell = document.createElement('td');
-        tableFooter.appendChild(tdCell);
-        tdCell.textContent = '?';
-    }
+            tdCell.textContent = '?';
+        }
 
     // need one cell for daily total
     const lastTotalCell = document.createElement('td');
@@ -211,22 +159,18 @@ function footerRow() {
 
 }
 
+
 headerRow();
 seattle.render();
 tokyo.render();
+paris.render();
 dubai.render();
-
+lima.render();
 footerRow();
 
 
-
-
-
-// ********* ESTIMATE TOTAL ***********
-/*
+function genHourlyTotal() {
 for(let i = 0; i < hours.length; i += 1) {
-
-    const timeSlot = hours[i];
 
     // total for each time slot
     let salesTotal = 0;
@@ -234,20 +178,20 @@ for(let i = 0; i < hours.length; i += 1) {
     for(let j = 0; j < standLocations.length; j += 1) {
 
         const currentStand = standLocations[j];
+        let sale = currentStand.hourlySales[i];
+        salesTotal += sale;
 
-        let salesCurrentStand = currentStand.hourlySales[timeSlot];
-
-        // tallys total sales for each timeslot
-        salesTotal += salesCurrentStand;
-        console.log('salesTotal: ' + salesTotal);
     }
 
     // breaks out of inner loop and pushes salesTotal to hourlyTotalSales
     hourlyTotalSales.push(salesTotal);
+
 }
 
 console.log('hourlTotalSales: ' + hourlyTotalSales);
 
-// do same as above for location totals
-const locationTotalSales = [];
-*/
+}
+
+
+const tdFooterElem = document.createElement('td');
+salesTableElem.appendChild(tdFooterElem);
